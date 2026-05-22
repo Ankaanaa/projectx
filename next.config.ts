78fa049afadaaac908e3e5630c3c180a185ef963
Dmitriy <dima.volkov1@icloud.com>
2026-05-22 17:56:44 +0300

    fix bugs

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: ['i.postimg.cc', 'i.porb'].map((domain) => ({
       protocol: 'https',
       hostname: domain,
     })),
   },
-}
+} satisfies NextConfig
 
 export default nextConfig
